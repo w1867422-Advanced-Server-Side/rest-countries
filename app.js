16ae456f93efc5818639ee2c4913335b8e8276cd
Tharusha Peiris <101708782+peiris830@users.noreply.github.com>
2025-04-18 10:45:32 +0530

    update app.js to mount api key routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./routes/authRoutes');
+const apiKeyRoutes = require('./routes/apiKeyRoutes');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/auth', authRoutes);
+app.use('/apiKey', apiKeyRoutes);
 
 app.get('/', (req, res) => {
     res.send( 'Server running on port: !' + PORT);
