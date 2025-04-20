a96aedf9e0833c5ee9a5f0d02d5525a32e24ac9b
Tharusha Peiris <101708782+peiris830@users.noreply.github.com>
2025-04-20 09:49:53 +0530

    enable CORS for port 5000 with credentials support

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors')
 
 const authRoutes = require('./routes/authRoutes');
 const apiKeyRoutes = require('./routes/apiKeyRoutes');
 const countryRoutes = require('./routes/countryRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(cors({
+    origin: 'http://localhost:5000',
+    credentials: true
+}));
+
 app.use('/auth', authRoutes);
 app.use('/apiKey', apiKeyRoutes);
 app.use('/countries', countryRoutes);
+app.use('/admin', adminRoutes);
 
 app.get('/', (req, res) => {
     res.send( 'Server running on port: !' + PORT);
